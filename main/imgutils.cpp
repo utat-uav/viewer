8561091718e53e769af91607da96e2865f4d8a14
David Koo <dkoo15@gmail.com>
2015-04-28 23:37:23 -0400

    implemented zbar to read QR codes

--- main/imgutils.cpp
+++ main/imgutils.cpp
@@ -51,6 +51,7 @@ namespace groundvision{
 			cv::imshow("Target",roiImg);
 			cv::waitKey(0);
 			cv::destroyWindow("Target");
+			readQR(roiImg);
 	}
 	
 	cv::Point map(cv::Point pnt, double size_factor){
@@ -115,5 +116,19 @@ namespace groundvision{
 		yc = (int) ytmp;	
 	}
 
+	void readQR(cv::Mat &roi){
+		cv::Mat gryscl;
+		cv::cvtColor(roi,gryscl,CV_RGB2GRAY);
+		unsigned char* data = (unsigned char*)(gryscl.data);
 
+		zbar::Image im(gryscl.cols, gryscl.rows, "Y800", data, gryscl.cols*gryscl.rows);
+		zbar::ImageScanner scanner;
+		scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE,1);
+		scanner.scan(im);
+
+		for(zbar::Image::SymbolIterator symbol = im.symbol_begin(); symbol!=im.symbol_end(); ++symbol){
+
+			std::cout<<symbol->get_data()<<std::endl;
+		}
+	}
 }
--- main/imgutils.h
+++ main/imgutils.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <zbar.h>
 
 #define SENS_WIDTH 24
 #define SENS_HEIGHT 18
@@ -12,6 +13,8 @@ namespace groundvision{
 	void makeTarget(cv::Mat &image, cv::Point pnt1, cv::Point pnt2);
 	cv::Point map(cv::Point pnt, double size_factor);
 	double calculateArea(int w, int h, std::vector<cv::Point> &poly, double altitude);	
-	void geolocate(int x, int y, int w, int h, double imlat, double imlon, double altitude, double heeading, double &targlat, double& targlon);
+	void geolocate(int x, int y, int w, int h, double imlat, double imlon, double altitude, double heading, double &targlat, double& targlon);
 	void centroid(std::vector<cv::Point> &poly, int& xc, int&yc);	
+
+	void readQR(cv::Mat &roi);
 }
